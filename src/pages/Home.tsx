d50056f72ae1abd6be9ca501a5685953a84a2831
Saeid Mohadjer <saeid@fastmail.fm>
2025-03-29 16:59:27 +0100

    minor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link } from 'react-router';
+// import { Link } from 'react-router';
 import './home.css';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
