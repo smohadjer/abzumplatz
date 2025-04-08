78719836aa8d1b2091e41f5c7d46bbbbf4b26b84
Saeid Mohadjer <saeid@fastmail.fm>
2025-04-08 13:03:48 +0200

    updates config.ts

--- api/_config.ts
+++ api/_config.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-let database_name = '';
+let database_name = 'abzumplatz';
 let database_uri = '';
 
 export const jwtSecret = process.env.jwtSecret;
@@ -10,12 +10,14 @@ export const environment = process.env.environment;
 
 if (environment === 'local') {
     database_uri = process.env.db_uri_local;
-    database_name = 'abzumplatz';
+}
+
+if (environment === 'stage') {
+    database_uri = process.env.db_uri_stage;
 }
 
 if (environment === 'prod') {
-    database_uri = process.env.db_uri_remote;
-    database_name = 'abzumplatz';
+    database_uri = process.env.db_uri_prod;
 }
 
 export { database_uri, database_name };
