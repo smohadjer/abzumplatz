1875b5b74ccb9b719976430ce81e1a4123f9d95d
Saeid Mohadjer <saeid@fastmail.fm>
2025-04-08 09:05:23 +0200

    fix date format

--- src/components/courts/Popup.tsx
+++ src/components/courts/Popup.tsx
@@ -7,12 +7,19 @@ export function Popup(props: {
     closePopup: MouseEventHandler;
     deleteReservation: MouseEventHandler;
 }) {
+    const getReservationDate = (date: string | undefined) => {
+        if (date) {
+            const day = new Date(date);
+            return day.toLocaleDateString('de-DE');
+        }
+    };
+
     return (
         <div className="lightbox">
             <div className="popup">
                 {/* {props.content.dataset.reservation_id} */}
                 <button disabled={props.disabled} className="close" onClick={props.closePopup}>X</button>
-                <p>Möchtest du deine Reservierung am {props.content.dataset.reservation_date} stornien?</p>
+                <p>Möchtest du deine Reservierung am {getReservationDate(props.content.dataset.reservation_date)} stornien?</p>
                 <button disabled={props.disabled} className="delete" onClick={props.deleteReservation}>Ja</button> {' '}
                 <button disabled={props.disabled} onClick={props.closePopup}>Nein</button>
                 {props.disabled ? <Loader /> : null}
