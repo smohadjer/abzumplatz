fe54b2b2940494819a3b460d4553011836f029d8
Saeid Mohadjer <saeid@fastmail.fm>
2025-07-10 10:49:21 +0200

    minor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router';
+import './home.css';
 
 export default function Home() {
     return (
         <div className="navbar">
-            <Link to="/login">Spieler Anmelden</Link>
-            <Link to="/register">Spieler Registrieren</Link>
-            <Link to="/register/club">Verein Registrieren</Link>
+            <Link to="/login">Anmelden
+                <span>wenn Sie sich bereits registriert haben</span>
+            </Link>
+            <Link to="/register">Registrieren
+                <span>Für Erstbenutzer</span>
+            </Link>
+            <Link to="/register/club">Verein Registrieren
+                <span>Für Clubadministratoren</span>
+            </Link>
         </div>
     )
 }
