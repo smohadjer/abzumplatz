a83690a7c8325fad0cf2c92e69cc9069fc6a7f1f
Saeid Mohadjer <saeid@fastmail.fm>
2025-10-22 19:31:30 +0200

    adds admin page

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -1,13 +1,21 @@
 import { Navigate } from 'react-router';
+import { useLocation } from 'react-router'
 
 type ProtectedRouteProps = {
     isLoggedin: boolean;
+    role: string;
     children: React.ReactNode;
 };
 
-export const ProtectedRoute = ({ isLoggedin, children }: ProtectedRouteProps) => {
+export const ProtectedRoute = ({isLoggedin, role, children}: ProtectedRouteProps) => {
+    const location = useLocation();
+
     if (!isLoggedin) {
-      return <Navigate to="/" replace />;
+        return <Navigate to="/" replace />;
+    }
+
+    if (role !== 'admin' && location.pathname === '/admin' ) {
+        return <Navigate to="/reservations" replace />;
     }
 
     return children;
