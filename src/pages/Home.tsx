70e8c21769de14944c812170ca49be8cd5a47db3
Saeid Mohadjer <saeid@fastmail.fm>
2025-12-04 09:19:27 +0100

    content styling update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,35 @@
 import { Link } from 'react-router';
-import { Slider } from '../components/slider/slider';
+// import { Slider } from '../components/slider/slider';
 import './home.css';
 
 export default function Home() {
-    const slides = [
-        {url: '/assets/1-min.jpg', text: 'Registrieren Sie sich als Vereinsadmin' },
-        {url: '/assets/2-min.jpg', text: 'Nach dem Einloggen wird Ihnen die Club-Registrierungsseite angezeigt'},
-        {url: '/assets/3-min.jpg', text: 'Sie und Ihre Vereinsmitglieder können jetzt Plätze buchen.'}
-    ];
+    // const slides = [
+    //     {url: '/assets/1-min.jpg', text: 'Registrieren Sie sich als Vereinsadmin' },
+    //     {url: '/assets/2-min.jpg', text: 'Nach dem Einloggen wird Ihnen die Club-Registrierungsseite angezeigt'},
+    //     {url: '/assets/3-min.jpg', text: 'Sie und Ihre Vereinsmitglieder können jetzt Plätze buchen.'}
+    // ];
 
     return (
         <>
             <div className="navbar">
-                <Link to="/login">Anmelden
-                    <span>Wenn Sie bereits registriert sind</span>
-                </Link>
+                <Link to="/login">Anmelden</Link>
                 <Link to="/register">Registrieren
-                    <span>Für Erstbenutzer</span>
+                    <span>Für Spieler und Vereinsadmin</span>
                 </Link>
                 {/* <Link to="/register/club">Verein Registrieren
                     <span>Für Clubadministratoren</span>
                 </Link> */}
             </div>
-            <p className="content">Abzumplatz ist eine Online-Plattform, die es Tennisvereinen ermöglicht, ihren Mitgliedern die kostenlose Reservierung von Plätzen anzubieten und alle Aspekte der Buchung über den Administrator abzuwickeln.</p>
-            <Slider slides={slides} />
+            <p className="intro">Abzumplatz ermöglicht es Tennisvereinen, ihren Mitgliedern kostenlose Online-Reservierungen ihrer Tennisplätze anzubieten.</p>
+            <img className="hero-image" src="/assets/1.png" alt="screenshot" />
+            {/* <Slider slides={slides} /> */}
             <div className="content">
-                <h3>Funktionen:</h3>
+                <h2>Funktionen für Vereinsadministratoren</h2>
                 <ul>
-                    <li>Einfache, intuitive Benutzeroberfläche, die auf allen Geräten funktioniert</li>
-                    <li>Kostenlose und unbegrenzte Buchung Ihrer Plätze für Vereinsmitglieder</li>
                     <li>Möglichkeit zur Festlegung verschiedener Reservierungsbeschränkungen</li>
                     <li>Wiederkehrende Buchungen für Training und andere Zwecke</li>
-                    <li>Sperrung von Plätzen für Mannschaftsspiele</li>
-                    <li>Echtzeit-Statistiken zu Ihren Mitgliedern und der Platznutzung direkt in der App</li>
+                    <li>Möglichkeit zum Sperren und Entsperren einzelner Tennisplätze</li>
+                    <li>Übersicht aller registrierten Mitglieder</li>
                 </ul>
                 <p className="tagline">Made mit ❤️ für 🎾 in Freiburg</p>
             </div>
