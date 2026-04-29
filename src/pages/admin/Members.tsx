079511fbac07de83fb4ed1fb5f5ffadc2bc5137e
Saeid Mohadjer <saeid@fastmail.fm>
2026-04-29 14:47:30 +0200

    adds status to users so admin can inactivate/activate users

--- src/pages/admin/Members.tsx
+++ src/pages/admin/Members.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, ChangeEventHandler} from "react";
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from './../../store';
 import { fetchUsers } from '../../utils/utils';
@@ -7,6 +7,7 @@ import { Link } from 'react-router';
 
 export default function AdminMembersPage() {
     const [loading, setLoading] = useState(false);
+    const [pending, setPending] = useState(false);
     const usersData = useSelector((state: RootState) => state.users);
     const user = useSelector((state: RootState) => state.auth);
     const dispatch = useDispatch();
@@ -22,6 +23,53 @@ export default function AdminMembersPage() {
         }
     }, []);
 
+    
+
+    const handleChange: ChangeEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.target.value = e.target.checked ? "active" : "inactive";
+        setPending(true);
+
+        fetch('/api/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                user_id: e.target.id,
+                status: e.target.value
+            })
+        }).then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error('Something went wrong!');
+            }
+        }).then(data => {
+            // update user status in store
+            const users = usersData.value.map(user => {
+                if (user._id === data._id) {
+                    return {
+                        ...user,
+                        status: data.status
+                    }
+                } else {
+                    return user;
+                }
+            });
+            dispatch({
+                type: 'users/fetch',
+                payload: {
+                    value: users,
+                    loaded: true
+                }
+            });
+            setPending(false);
+        }).catch(e => {
+            console.error(e);
+            setPending(false);
+        });
+    }
+
     return (
         loading ? (
             <div className="splash">
@@ -30,9 +78,19 @@ export default function AdminMembersPage() {
         ) : (
             <>
                 <Link className="icon icon--back" to="/admin">Zurück</Link>
-                <h2>Mitgliederliste ({users.length})</h2>
+                <h2><span style={{ marginRight: '0.5rem' }}>Mitgliederliste ({users.length})</span>
+                    {pending ? <Loader size="small" /> : null}
+                </h2>
                 <ul className="users-list">
-                    {users.map(user => <li key={user._id}>{user.first_name} {user.last_name}</li>)}
+                    {users.map(user => {
+                        return <li key={user._id}>
+                            <label htmlFor={user._id}>
+                                <input id={user._id} type="checkbox" defaultChecked={user.status === "active"} onChange={handleChange} />
+                                {user.first_name} {user.last_name}
+                            </label>
+                            <span style={{ marginLeft: '0.25rem' }}>(<a href={`mailto:${user.email}`}>{user.email}</a>)</span>
+                        </li>;
+                    })}
                 </ul>
             </>
         )
