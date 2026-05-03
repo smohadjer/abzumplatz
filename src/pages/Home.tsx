82727ce05d3fb04c087b0310be23cc160c08a67a
Saeid Mohadjer <saeid@fastmail.fm>
2026-05-03 21:31:32 +0200

    minor content and styling update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,12 +22,24 @@ export default function Home() {
             <img className="hero-image" src="/assets/1.png" alt="screenshot" />
             {/* <Slider slides={slides} /> */}
             <div className="content">
-                <h2>Funktionen für Vereinsadministratoren</h2>
+                <h2>Was abzumplatz Ihnen bietet:</h2>
                 <ul>
-                    <li>Möglichkeit zur Festlegung verschiedener Reservierungsbeschränkungen</li>
-                    <li>Wiederkehrende Buchungen für Training und andere Zwecke</li>
-                    <li>Möglichkeit zum Sperren und Entsperren einzelner Tennisplätze</li>
-                    <li>Übersicht aller registrierten Mitglieder</li>
+                <li>Kostenlose Online-Platzreservierung für Tennisvereine</li>
+                <li>Einfache Registrierung für Vereine und Spieler</li>
+                <li>Übersichtlicher Buchungskalender für alle Tennisplätze</li>
+                <li>Schnelle Platzbuchung für Mitglieder</li>
+                <li>Anzeige eigener Buchungen</li>
+                <li>Verwaltung von Vereinsmitgliedern</li>
+                <li>Aktivieren und Deaktivieren von Spielern</li>
+                <li>Flexible Verwaltung der Anzahl verfügbarer Plätze</li>
+                <li>Sperren und Freigeben einzelner Tennisplätze</li>
+                <li>Einstellbare Reservierungszeiten pro Verein</li>
+                <li>Begrenzung aktiver Buchungen pro Spieler</li>
+                <li>Wiederkehrende Buchungen für Training und regelmäßige Termine</li>
+                <li>Erweiterte Buchungsrechte für Vereinsadministratoren</li>
+                <li>Zentrale Übersicht aller Reservierungen im Verein</li>
+                <li>Club-spezifische Daten und Berechtigungen</li>
+                <li>Mobile-freundliche Nutzung im Browser</li>
                 </ul>
             </div>
         </>
