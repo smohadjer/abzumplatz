03b93b5477b513b280a158cec1968d9281c8e1e4
Saeid Mohadjer <saeid@fastmail.fm>
2026-05-14 16:32:38 +0200

    refactors calendar header

--- src/components/courts/Calendar.tsx
+++ src/components/courts/Calendar.tsx
@@ -36,23 +36,27 @@ export function Calendar(props: Props) {
 
     return (
         <div className="header">
-            <span onClick={reload} className="icon icon--inline icon--reload"></span>
-            <button
-                onClick={prevDay}
-                className="prev">&lt;</button>
-            <span className="date-picker">
-                <span className="shortday">{new Date(isoDate).toLocaleDateString('de-DE', {weekday: 'short'}).toUpperCase()}</span>
-                <input type="date"
-                value={isoDate}
-                onChange={e => setReservationDate(new Date(e.target.value))} />
-            </span>
-            <button
-                onClick={nextDay}
-                className="next">&gt;</button>
-            <button
-                disabled={disabled}
-                onClick={today}
-                className="today">Heute</button>
+            <div className="header_refresh">
+                <span onClick={reload} className="icon icon--inline icon--reload">Neu laden</span>
+                <button
+                    disabled={disabled}
+                    onClick={today}
+                    className="today">Heute</button>
+            </div>
+            <div className="header_datepicker">
+                <button
+                    onClick={prevDay}
+                    className="prev">&lt;</button>
+                <span className="date-picker">
+                    <span className="shortday">{new Date(isoDate).toLocaleDateString('de-DE', {weekday: 'short'}).toUpperCase()}</span>
+                    <input type="date"
+                    value={isoDate}
+                    onChange={e => setReservationDate(new Date(e.target.value))} />
+                </span>
+                <button
+                    onClick={nextDay}
+                    className="next">&gt;</button>
+            </div>
         </div>
     )
 }
