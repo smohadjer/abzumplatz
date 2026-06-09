ffcab838b67e11ad78c4eb5767d2947092043dbe
Saeid Mohadjer <saeid@fastmail.fm>
2026-06-09 11:38:27 +0200

    Redirect users without club from reservation routes

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -11,18 +11,22 @@ export const ProtectedRoute = ({children}: ProtectedRouteProps) => {
     const location = useLocation();
     const auth = useSelector((state: RootState) => state.auth);
     const isLoggedin = auth.value;
+    const isReservationRoute = ['/reservations', '/bookings'].includes(location.pathname);
     // console.log('ProtectedRoute', {isLoggedin}, {role}, location.pathname)
 
     if (!isLoggedin) {
         return <Navigate to="/" replace />;
     } else {
         if (auth.role !== 'admin' && location.pathname.startsWith('/admin')) {
-            // console.log('redirecting non-admin user to /reservations page');
             return <Navigate to="/reservations" replace />;
         }
 
         if (location.pathname === '/select-club' && auth.role === 'admin') {
             return <Navigate to="/admin" replace />;
+        }
+
+        if (auth.role !== 'admin' && isReservationRoute && !auth.club_id) {
+            return <Navigate to="/select-club" replace />;
         }
 
         return children;
