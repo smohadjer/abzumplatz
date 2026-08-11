a5cc21e1ee3e4ca3eb8ee7053f906eda82a0fe75
Saeid Mohadjer <saeid@fastmail.fm>
2026-08-11 12:39:42 +0200

    Update clubRules.ts

--- src/clubRules.ts
+++ src/clubRules.ts
@@ -2,6 +2,7 @@ export const defaultClubRules = [
     'Nur Vereinsmitglieder dürfen die Tennisplätze reservieren und darauf spielen. Gäste können in Begleitung eines Vereinsmitglieds gegen eine Gebühr und in begrenztem Umfang ebenfalls spielen.',
     'Erscheint innerhalb der ersten 10 Minuten nach Beginn der Reservierungszeit kein Spieler auf dem Platz, gilt die Reservierung als storniert. In diesem Fall kann jeder Spieler den Platz für die verbleibende Zeit bis zur nächsten Reservierung nutzen.',
     'Die Spieler sollten ihr Spiel mindestens 5 Minuten vor Ablauf der Reservierungszeit beenden, damit sie genügend Zeit haben, den Platz zu wässern und abzuziehen.',
+    'Wenn Sie einen Platz reserviert haben und feststellen, dass Sie ihn nicht wahrnehmen können, löschen Sie Ihre Reservierung so bald wie möglich, damit andere Spieler diesen Platz buchen können.',
     'Bitte betretet die Tennisplätze ausschließlich mit Tennisschuhen!',
     'Wenn der Platz trocken ist, sollten Sie ihn gründlich wässern. Je nach Temperatur wässern Sie den Platz bitte vor Spielbeginn, nach einem Match und – falls erforderlich – in der Satzpause.',
 ];
